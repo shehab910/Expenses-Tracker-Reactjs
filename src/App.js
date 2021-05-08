8552009c0d61dd52908cf3a71a93f933cc708aab
Shehab <shehabs.910@gmail.com>
2021-05-09 00:22:25 +0200

    016. splitted existing compenents into new ones (expenses and card)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import ExpenseItem from './components/ExpenseItem'
+import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
-  
   const expenses = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
-  
-  return (
-    <div className="App">
-      {
-        expenses.map((expense) => {
-          return(<ExpenseItem date={expense.date} title={expense.title} price={expense.amount} />);
-        })
-      }
 
-      {/* <ExpenseItem date={expenses[0].date} title={expenses[0].title} price={expenses[0].amount} />
-      <ExpenseItem date={expenses[1].date} title={expenses[1].title} price={expenses[1].amount} />
-      <ExpenseItem date={expenses[2].date} title={expenses[2].title} price={expenses[2].amount} />
-      <ExpenseItem date={expenses[3].date} title={expenses[3].title} price={expenses[3].amount} /> */}
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,23 @@
+import ExpenseItem from './ExpenseItem';
+import Card from './Card'
+import './Expenses.css';
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {
+        props.items.map((expense) => {
+          return(<ExpenseItem date={expense.date} title={expense.title} price={expense.amount} />);
+        })
+      }
+
+      {/* <ExpenseItem date={expenses[0].date} title={expenses[0].title} price={expenses[0].amount} />
+      <ExpenseItem date={expenses[1].date} title={expenses[1].title} price={expenses[1].amount} />
+      <ExpenseItem date={expenses[2].date} title={expenses[2].title} price={expenses[2].amount} />
+      <ExpenseItem date={expenses[3].date} title={expenses[3].title} price={expenses[3].amount} /> */}
+
+    </Card>
+  );
+}
+
+export default Expenses;
