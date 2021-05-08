53e03db62a7c141c48c0f4e430ddaf0fce725452
Shehab <shehabs.910@gmail.com>
2021-05-08 23:46:45 +0200

    015. splitted ExpenseDate from ExpenseItem, added its css

--- src/App.js
+++ src/App.js
@@ -26,11 +26,16 @@ function App() {
   
   return (
     <div className="App">
-      <h1>hi....</h1>
-      <ExpenseItem date={expenses[0].date} title={expenses[0].title} price={expenses[0].amount} />
+      {
+        expenses.map((expense) => {
+          return(<ExpenseItem date={expense.date} title={expense.title} price={expense.amount} />);
+        })
+      }
+
+      {/* <ExpenseItem date={expenses[0].date} title={expenses[0].title} price={expenses[0].amount} />
       <ExpenseItem date={expenses[1].date} title={expenses[1].title} price={expenses[1].amount} />
       <ExpenseItem date={expenses[2].date} title={expenses[2].title} price={expenses[2].amount} />
-      <ExpenseItem date={expenses[3].date} title={expenses[3].title} price={expenses[3].amount} />
+      <ExpenseItem date={expenses[3].date} title={expenses[3].title} price={expenses[3].amount} /> */}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
+      {/*<div>{props.date.toISOString()}</div>*/}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
